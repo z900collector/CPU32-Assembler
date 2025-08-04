aacf1ad961530fd80da4e8bdeea6552723648169
Sid Young <z900collector@users.noreply.github.com>
2025-08-04 13:15:01 +1000

    Create LDInst.h

---
+++ src/instructions/LDInst.h
@@ -0,0 +1,23 @@
+#ifndef LOAD_INST_H
+#define LOAD_INST_H
+
+#include <string>
+#include <vector>
+
+#include "Instruction.h"
+#include "Label.h"
+#include "Logger.h"
+
+
+class LDInst : public Instruction
+{
+protected:
+Logger *pLog;
+Instruction *LoadRegister(std::vector<std::string>);
+Instruction *LoadImmediate(std::vector<std::string>);
+
+public:
+LDInst();
+Instruction * parse(std::vector<std::string>);
+};
+#endif
