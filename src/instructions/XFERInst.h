b5545d7b9ec591971a3f43f74e7c81fdf5c48673
Sid Young <z900collector@users.noreply.github.com>
2025-08-04 13:26:47 +1000

    Create XFERInst.h

---
+++ src/instructions/XFERInst.h
@@ -0,0 +1,22 @@
+#ifndef XFER_INST_H
+#define XFER_INST_H
+
+#include <string>
+#include <vector>
+
+#include "Instruction.h"
+#include "Label.h"
+#include "Logger.h"
+
+
+class XFERInst : public Instruction
+{
+protected:
+Logger *pLog;
+Instruction *XferToRegister(std::vector<std::string>);
+
+public:
+XFERInst();
+Instruction * parse(std::vector<std::string>);
+};
+#endif
