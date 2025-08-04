433c842fb83956dffc446330af82d87354982efa
Sid Young <z900collector@users.noreply.github.com>
2025-08-04 13:07:27 +1000

    Create INCDECInst.h

---
+++ src/instructions/INCDECInst.h
@@ -0,0 +1,33 @@
+/**
+ *
+ * Process INC & DEC instructions
+ *
+ * Format:
+ *  DEC Rd   where "d" is 0-8
+ *  DEC (Rd) memory pointed to by Register
+ */
+
+#ifndef INC_DEC_INST_Header
+#define INC_DEC_INST_Header
+
+#include <string>
+#include <vector>
+
+#include "Instruction.h"
+#include "Label.h"
+#include "Logger.h"
+
+
+class INCDECInst : public Instruction
+{
+protected:
+Logger *pLog;
+Instruction * Decrement(std::vector<std::string>);
+Instruction * Increment(std::vector<std::string>);
+
+
+public:
+INCDECInst();
+Instruction * parse(std::vector<std::string>);
+};
+#endif
