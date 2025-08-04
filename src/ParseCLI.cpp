0cb871872aeb891836400cb29e7d9abaf51da212
Sid Young <z900collector@users.noreply.github.com>
2025-08-04 11:48:35 +1000

    Create ParseCLI.cpp

---
+++ src/ParseCLI.cpp
@@ -0,0 +1,89 @@
+/**
+ * Parse the CLI arguments
+ *
+ * Format
+ * spasm <inputfile> -n -d
+ * -o <outputfile (default output is <inputfile>.bin
+ * -v  verbose output
+ *
+ * (C) Sid Young
+ * Free for non-commercial use.
+ */
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <ostream>
+#include <iostream>
+#include "GlobalParameters.h"
+#include "ParseCLI.h"
+
+using namespace std;
+
+ParseCLI::ParseCLI( GlobalParameters *p )
+{
+        this->pGP = p;
+}
+
+
+
+void ParseCLI::parse(int argc, char* argv[])
+{
+bool _output_file_flag = false;
+
+
+        if(argc > 64)
+        {
+                throw std::out_of_range (" too many input parameters! ");
+        }
+        vector<string> args(argv + 1, argv + argc);
+
+/*
+        for (vector<string>::iterator t = args.begin(); t != args.end(); t++)
+        {
+                cout<<"ARG: "<< *t << endl;
+        }
+*/
+
+
+        for(const auto& arg : args)
+        {
+                if(this->pGP->getInputFile().empty())
+                {
+                        if(arg == "-n")
+                        {
+                                if(this->pGP->getLineNumberFlag()==true)
+                                {
+                                        throw std::runtime_error(" cannot use -n parameter twice!");
+                                }
+                                this->pGP->setLineNumberFlag(true);
+                                continue;
+                        }
+
+                        if(arg == "-d" || arg == "--dump")
+                        {
+                                if(this->pGP->getDumpFlag() == true)
+                                {
+                                        throw std::runtime_error(" cannot use -d/--dump parameter twice!");
+                                }
+                                this->pGP->setDumpFlag(true);
+                                continue;
+                        }
+                }
+                else
+                {
+                        if(_output_file_flag == true)
+                        {
+                                this->pGP->setOutputFile(arg);
+                                continue;
+                        }
+                        if((arg == "-o") && (_output_file_flag == false))
+                        {
+                                _output_file_flag = true;
+                                continue;
+                        }
+                }
+                this->pGP->setInputFile(arg);
+        }
+}
+/* End of file */
