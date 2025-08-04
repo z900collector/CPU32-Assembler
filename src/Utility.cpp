ba7b28d85dbb13bd23533dd5bfc30045b43de6a8
Sid Young <z900collector@users.noreply.github.com>
2025-08-04 12:58:41 +1000

    Create Utility.cpp

---
+++ src/Utility.cpp
@@ -0,0 +1,112 @@
+/**
+ * Various utility functions common to many operations but not all!
+ *
+ *
+ *
+ * (C) Sid Young
+ * Free for non-commercial use.
+ */
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <regex>
+#include <algorithm>
+#include <iomanip>
+
+#include "Utility.h"
+
+
+
+/**
+ *------------------------------------------------------------
+ *
+ * Rotate the register index till its in the right place.
+ * This type of Utility has Source and Destination Registers
+ * encoded in the same instruction at different spots.
+ *
+ */
+unsigned int Utility::getRegisterMask(char reg, unsigned int mask)
+{
+        switch(reg)
+        {
+                case 'D':
+                        mask = mask << 16;
+                        break;
+                case 'S':
+                        mask = mask << 20;
+                        break;
+        }
+        return mask;
+}
+
+
+
+/**
+ *------------------------------------------------------------
+ *
+ * Given a string representing the register as R1, R2 etc
+ * return an INT 0-x
+ * This RISC design used R0 to R8
+ *
+ */
+unsigned int Utility::getRegister(std::string r_name)
+{
+unsigned int rv = 0;
+
+        char r_index = r_name.at(1);
+
+        if(r_name.at(0) == 'R')
+        {
+                rv = int(r_index)-48;
+        }
+        else
+        {
+                throw std::invalid_argument("Registers should start with R");
+        }
+        return rv;
+}
+
+
+
+/**
+ *------------------------------------------------------------
+ *
+ * One way to trim the LHS of a string
+ *
+ */
+std::string Utility::ltrim(std::string str)
+{
+        const std::string nothing = "";
+        const auto line = std::regex_replace(str, std::regex( "^\\s+" ), nothing);
+        return line;
+}
+
+
+
+std::string Utility::rtrim(std::string str)
+{
+        const std::string nothing = "";
+        const auto trimmed = std::regex_replace(str, std::regex( "\\s+$" ), nothing);
+        return trimmed;
+}
+
+
+/**
+ *------------------------------------------------------------
+ *
+ * Given a string, split it into tokens
+ *
+ */
+std::vector<std::string> Utility::split(const std::string& str)
+{
+std::vector<std::string> tokens;
+std::istringstream iss(str);
+std::string token;
+
+        while (iss >> token)
+        {
+                tokens.push_back(token);
+        }
+        return tokens;
+}
+/* End of file */
