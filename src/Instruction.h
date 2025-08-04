aa9fb80c86d4c9ff09844b139afa847c9b631e22
Sid Young <z900collector@users.noreply.github.com>
2025-08-04 11:00:14 +1000

    Create Instruction.h

---
+++ src/Instruction.h
@@ -0,0 +1,37 @@
+/**
+ * Base class for all Instructions
+ */
+#ifndef Instruction_Header
+#define Instruction_Header
+
+#include <string>
+#include <vector>
+//#include "Label.h"
+
+
+
+class Instruction
+{
+protected:
+std::string   instruction_name="";
+unsigned int  instruction_word=0;
+unsigned int  instruction_length=1;
+unsigned int  _mem_loc;
+bool          _mem_loc_flag;
+
+public:
+std::string getName();
+void setName(std::string);
+unsigned int getWord();
+void setWord(unsigned int);
+unsigned int getLength();
+void setLength(unsigned int);
+unsigned int getLocation();
+void setLocation(unsigned int);
+std::string toHex();
+
+virtual Instruction * parse(std::vector<std::string>)=0;
+//virtual Instruction * parse(std::vector<std::string>, std::vector<Label *> cLabels, std::vector<Instruction *> MissingLabels);
+};
+
+#endif
