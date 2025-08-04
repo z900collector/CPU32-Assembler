7b3183c3cae306696474abdafd1514c8eff61847
Sid Young <z900collector@users.noreply.github.com>
2025-08-04 11:08:24 +1000

    Create Instruction.cpp

---
+++ src/Instruction.cpp
@@ -0,0 +1,33 @@
+/**
+ * Common "Instruction" fields.
+ *
+ *
+ *
+ * (C) Sid Young 2025
+ * Free for non-commercial use.
+ */
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include "Instruction.h"
+
+
+std::string Instruction::getName() { return this->instruction_name;  }
+void Instruction::setName(std::string name) { this->instruction_name=name;  }
+unsigned int Instruction::getWord() { return this->instruction_word; }
+void Instruction::setWord(unsigned int iw) { this->instruction_word=iw; }
+unsigned int Instruction::getLength() { return this->instruction_length; }
+void Instruction::setLength(unsigned int len) { this->instruction_length=len; }
+unsigned int Instruction::getLocation(){ return this->_mem_loc; }
+void Instruction::setLocation(unsigned int loc) {this->_mem_loc = loc;this->_mem_loc_flag=true; }
+
+
+std::string Instruction::toHex()
+{
+std::stringstream ss;
+        ss << std::setw(8) << std::setfill('0') <<std::hex << this->getWord();
+        return ss.str();
+}
+
+/* End of file */
