fb68ac318b21123566e763287a3d9aaf1201db0c
Sid Young <z900collector@users.noreply.github.com>
2025-08-04 11:30:29 +1000

    Create InstSet.h

---
+++ src/InstSet.h
@@ -0,0 +1,43 @@
+/**
+ * Below is the 16 bit version of the CPU, it will be 24 bits when complete.
+ *
+ * The design of the CPU uses the top 2 bits to define the instruction type for the four parallel instruction registers.
+ * 00------ Misc Instructions and Primary ALU Operations.
+ * 01------ Registrer Moves involving memory/Immediate values
+ * 10------ Transfer Register and Register specific ALU oPerations
+ * 11------ Jump/Call Instructions + HALT (all F's)
+ */
+#ifndef INSTRUCTION_CODES_HDR
+#define INSTRUCTION_CODES_HDR
+
+/**
+ * Instruction mnemonic bit encoding
+ * can be used in custom architecture
+ *
+ */
+
+enum InstSet
+{
+        NOP   = 0x0000,
+        BSET  = 0x0100,
+        BCLR  = 0x0200,
+        BTST  = 0x0300,
+        DEC_R = 0x1100,
+        DEC_M = 0x1200,
+        INC_R = 0x2000,
+        INC_M = 0x2100,
+        XOR_R = 0x3000,
+        XOR_RM= 0x3100,
+        XOR_M = 0x3200,
+        JMP   = 0xc100,
+        JR    = 0xc200,
+        JC    = 0xc300,
+        JNC   = 0xc4100,
+        JZ    = 0xc500,
+        JNZ   = 0xc600,
+        LD    = 0x7000,
+        LDM   = 0x7100,
+        LDI   = 0x7200,
+        HALT  = 0xff00
+};
+#endif
