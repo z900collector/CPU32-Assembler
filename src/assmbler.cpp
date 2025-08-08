eb5d669199fd4ad96c4dfda1dced1712d13f8670
Sid Young <sid.young@gmail.com>
2025-08-08 11:25:45 +1000

    Code cleanup

--- src/assmbler.cpp
+++ src/assmbler.cpp
@@ -67,52 +67,49 @@ int main(int argc, char *argv[])
 {
 fstream _srcFile;
 
-        Logger *pLog = Logger::getInstance();
-        pLog->setLogDir("/logs");
-        pLog->setFileName("assembler-debug");
-        pLog->Start();
-        GlobalParameters *pGP = GlobalParameters::getInstance();
-        pLog->LogMsg("Create Parser - parse CLI");
-        ParseCLI *pCLI = new ParseCLI( pGP );
-        try {
-                pCLI->parse(argc, argv);
-        } catch (const std::exception &x)
-        {
-                pLog->LogMsg("ERROR - CLI Parser failed");
-                std::cerr << "ERROR: " << x.what() << '\n';
-                std::cerr << "usage: spasm -n -d <input_file> [-o <output_file>]...\n";
-                std::cerr << "       options first, then files <opt> <input_file> [-o <output_file>]...\n";
-                std::cerr << "       <opt> <input_file> [-o <output_file>]...\n";
-                return EXIT_FAILURE;
-        }
-        pLog->LogMsg( "Source file ["+pGP->getInputFile()+"]" );
-        pLog->LogMsg( "  Output to ["+pGP->getOutputFile()+"]" );
-//      pLog->LogMsg( "Dump Labels ["+pGP->getDumpFlag()+"]" );
-//      pLog->LogMsg( "Line Numbers["+pGP->getLineNumberFlag()+"]" );
+	Logger *pLog = Logger::getInstance();
+	pLog->setLogDir("/logs");
+	pLog->setFileName("assembler-debug");
+	pLog->Start();
+	GlobalParameters *pGP = GlobalParameters::getInstance();
 
-        _srcFile.open(pGP->getInputFile(), ios::in);
-        if(_srcFile.is_open())
-        {
-                pLog->LogMsg( "File opened." );
-					 cout<<"Open file [ "<<pGP->getInputFile()<<" ]"<<endl;
-                Parser *pParser = new Parser();
-                const auto pIList = pParser->parse(&_srcFile, pGP);
-                pLog->LogMsg( "Close File." );
-                _srcFile.close();
-                delete pParser;
-        }
-        else
-        {
-                pLog->LogMsg( "ERROR - File Not Found." );
-                cerr << "ERROR - file not found - Unable to open file!" << endl;
-        }
+	pLog->LogMsg("Create Parser - parse CLI");
+	ParseCLI *pCLI = new ParseCLI( pGP );
+	try {
+			  pCLI->parse(argc, argv);
+	} catch (const std::exception &x)
+	{
+		pLog->LogMsg("ERROR - CLI Parser failed");
+		std::cerr << "ERROR: " << x.what() << '\n';
+		std::cerr << "usage: asm24 -n -d <input_file> [-o <output_file>]...\n";
+		std::cerr << "       options first, then files <opt> <input_file> [-o <output_file>]...\n";
+		std::cerr << "       <opt> <input_file> [-o <output_file>]...\n";
+		return EXIT_FAILURE;
+	}
+	pLog->LogMsg( "Source file ["+pGP->getInputFile()+"]" );
+	pLog->LogMsg( "  Output to ["+pGP->getOutputFile()+"]" );
+	pLog->LogMsg( "Dump Labels ["+pGP->getDumpFlag()+"]" );
+	pLog->LogMsg( "Line Numbers["+pGP->getLineNumberFlag()+"]" );
 
-        delete pCLI;
-        pLog->End();
-        return 0;
+	_srcFile.open(pGP->getInputFile(), ios::in);
+	if(_srcFile.is_open())
+	{
+		pLog->LogMsg( "File opened." );
+		cout<<"Open file [ "<<pGP->getInputFile()<<" ]"<<endl;
+		Parser *pParser = new Parser();
+		const auto pIList = pParser->parse(&_srcFile, pGP);
+		pLog->LogMsg( "Close File." );
+		_srcFile.close();
+		delete pParser;
+	}
+	else
+	{
+		pLog->LogMsg( "ERROR - File Not Found." );
+		cerr << "ERROR - file not found - Unable to open file!" << endl;
+	}
+	delete pCLI;
+	pLog->End();
+	return 0;
 }
+
 /* End of file */
-
-
-
-
